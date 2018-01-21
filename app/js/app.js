2ae4976b3aa01b24b7dd006e9b70d2a6cd81a6ab
Adam Mathieson <adam.mathieson04@gmail.com>
2018-01-21 20:04:23 +0100

    F7V2

---
+++ app/js/app.js
@@ -0,0 +1,195 @@
+// Dom7
+var $$ = Dom7;
+
+// Framework7 App main instance
+var app = new Framework7({
+    root: '#app', // App root element
+    id: 'eu.mathieson.adam.genav', // App bundle ID
+    name: 'GéNav', // App name
+    theme: 'auto', // Automatic theme detection
+    routes: routes,
+    view: {
+        pushState: true
+    }
+});
+
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+    url: '/'
+});
+var currentLang = "EN";
+
+function title2TPGApp() {
+    // document.getElementById("title").innerHTML = "GéNav";
+    // document.getElementById("navbar").style.color = "#fff";
+    // document.getElementById("navbar").style.backgroundColor = "#f44336";
+    try {
+        StatusBar.backgroundColorByHexString("#ba000d");
+        window.plugins.headerColor.tint("#f44336");
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+function refreshDeps() {
+    $.get("../hosted_app-V2/stopsASYNC.php?stop=" + document.getElementById("currStopCode").innerHTML + "&off=0&len=50",function (data) {
+        var depsVlist = app.virtualList.create({
+            // List Element
+            el: '.depsList',
+            // Pass array with items
+            items: JSON.parse(data),
+            // Custom search function for searchbar
+            searchAll: function (query, items) {
+                var found = [];
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].destination.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].line.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].wifi.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].USB.toLowerCase().indexOf(query.toLowerCase()) >= 0 || query.trim() === '') found.push(i);
+                }
+                return found; //return array with mathceds indexes
+            },
+
+            // List item Template7 template
+            itemTemplate:
+            '<li style="background-color: #{{primCol}}; color: #{{txtCol}};">' +
+            '<a href="/dep/?dc={{depCde}}&col={{primCol}}&colT={{txtCol}}&nav={{navBar}}" onclick="document.getElementById(\'currDepCode\').innerHTML = \'{{depCde}}\';' +
+            'app.preloader.show(translateStr(\'loading\',currentLang));' +
+            'setTimeout(function () {app.preloader.hide();}, 1000);' +
+            'StatusBar.backgroundColorByHexString(\'#{{secCol}}\');' +
+            'window.plugins.headerColor.tint(\'#{{promCol}}\');" class="item-link item-content">' +
+            '<div class="item-inner">' +
+            '<div class="item-title-row" style="position: relative; top: -5px;">' +
+            '<div class="item-title">{{line}}&nbsp;&nbsp;<i style="position: relative; top: 4px;" class="f7-icons">arrow_right</i>&nbsp;&nbsp;{{destination}}&nbsp;<i class="material-icons wifi">{{wifi}}</i>&nbsp;<i class="material-icons wifi">{{USB}}</i>{{DATTO}}{{DATTM}}</div>' +
+            '<div class="item-after"style="color: #{{txtCol}};">{{#if arrived}}<i class="material-icons blinker">directions_bus</i>{{else}}{{time}}{{/if}}</div>' +
+            '</div>' +
+            '</div>' +
+            '</a>' +
+            '</li>',
+            // Item height
+            height: app.theme === 'ios' ? 63 : 73,
+            updatableScroll: true
+        });
+    });
+    setTimeout(function () {
+        refreshDeps();
+    }, 10000);
+}
+
+title2TPGApp();
+// MD
+app.on('pageInit', function (e) {
+    // do something on page init
+    if (e.name == "home") {
+        title2TPGApp();
+        $.get(hostedDir() + "/disruptions-home.php", function (data, status) {
+                document.getElementsByClassName("homeDis")[0].innerHTML = data;
+            }
+        );
+        $.get(hostedDir() + "/disruptions-home.php", function (data, status) {
+            var els = document.getElementsByClassName("homeDis");
+
+            Array.prototype.forEach.call(els, function (el) {
+                // Do stuff here
+                el.innerHTML = data;
+            });
+        });
+    }
+    if (e.name == "times") {
+        var stopsVlist = app.virtualList.create({
+            // List Element
+            el: '.stops',
+            // Pass array with items
+            items: JSON.parse(document.getElementById("stops").innerHTML),
+            // Custom search function for searchbar
+            searchAll: function (query, items) {
+                var found = [];
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].code.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].line.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].stopNoChars.toLowerCase().indexOf(query.toLowerCase()) >= 0 || query.trim() === '') found.push(i);
+                }
+                return found; //return array with mathced indexes
+            },
+
+            // List item Template7 template
+            itemTemplate:
+            '<li>' +
+            '<a href="/stop/?stop={{code}}&off=0&len=20" onclick="app.preloader.show(translateStr(\'loading\',currentLang));' +
+            'setTimeout(function () {app.preloader.hide();}, 1000); ' +
+            'document.getElementById(\'currStopCode\').innerHTML = \'{{code}}\';' +
+            'document.getElementById(\'currStopName\').innerHTML = \'{{name}}\'" class="item-link item-content">' +
+            '<div class="item-inner">' +
+            '<div class="item-title-row">' +
+            '<div class="item-title">{{name}}</div>' +
+            '{{#if DATTM}}<span class="badge color-green">New Ticket Machine</span>{{else}}{{/if}} {{#if DATTO}}<span class="badge color-blue">Old Ticket Machine</span>{{else}}{{/if}}' +
+            '</div>' +
+            '<div class="item-subtitle">{{code}}</div>' +
+            '</div>' +
+            '</a>' +
+            '</li>',
+            // Item height
+            height: app.theme === 'ios' ? 63 : 73,
+            updatableScroll: true
+        });
+    }
+    if (e.name == "stop") {
+        var depsVlist = app.virtualList.create({
+            // List Element
+            el: '.depsList',
+            // Pass array with items
+            items: JSON.parse(document.getElementById("deps").value),
+            // Custom search function for searchbar
+            searchAll: function (query, items) {
+                var found = [];
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].destination.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].line.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].wifi.toLowerCase().indexOf(query.toLowerCase()) >= 0 || items[i].USB.toLowerCase().indexOf(query.toLowerCase()) >= 0 || query.trim() === '') found.push(i);
+                }
+                return found; //return array with mathceds indexes
+            },
+
+            // List item Template7 template
+            itemTemplate:
+            '<li style="background-color: #{{primCol}}; color: #{{txtCol}};">' +
+            '<a href="/dep/?dc={{depCde}}&col={{primCol}}&colT={{txtCol}}&nav={{navBar}}" onclick="document.getElementById(\'currDepCode\').innerHTML = \'{{depCde}}\';' +
+            'app.preloader.show(translateStr(\'loading\',currentLang));' +
+            'setTimeout(function () {app.preloader.hide();}, 1000);' +
+            'StatusBar.backgroundColorByHexString(\'#{{secCol}}\');' +
+            'window.plugins.headerColor.tint(\'#{{promCol}}\');" class="item-link item-content">' +
+            '<div class="item-inner">' +
+            '<div class="item-title-row" style="position: relative; top: -5px;">' +
+            '<div class="item-title">{{line}}&nbsp;&nbsp;<i style="position: relative; top: 4px;" class="f7-icons">arrow_right</i>&nbsp;&nbsp;{{destination}}&nbsp;<i class="material-icons wifi">{{wifi}}</i>&nbsp;<i class="material-icons wifi">{{USB}}</i>{{DATTO}}{{DATTM}}</div>' +
+            '<div class="item-after"style="color: #{{#if pink}}ff00ff{{else}}{{txtCol}}{{/if}};">{{#if arrived}}<i class="material-icons blinker">directions_bus</i>{{else}}{{time}}{{/if}}</div>' +
+            '</div>' +
+            '</div>' +
+            '</a>' +
+            '</li>',
+            // Item height
+            height: app.theme === 'ios' ? 63 : 73,
+            updatableScroll: true
+        });
+        refreshDeps();
+    }
+    if (e.name == "directionsRTN") {
+        dirMap("HOME");
+    }
+
+
+});
+// IOS
+$$(document).on('page:init', function (e, page) {
+    console.log(page);
+})
+function dirMap(id) {
+    var a = document.getElementsByClassName("trajet-map");
+    for (index = 0; index < a.length; ++index) {
+        //console.log(a[index].src.replace("SCREENWIDTH", screen.width));
+        a[index].src = a[index].src.replace("SCREENWIDTH", screen.width);
+    }
+    var a = document.getElementsByClassName("trajet-map");
+    for (index = 0; index < a.length; ++index) {
+        a[index] = a[index].setAttribute("hidden","hidden");
+    }
+    document.getElementById("journey_" + id).removeAttribute("hidden");
+}
+function submitDir() {
+    var origin = document.getElementById("originDir");
+    var desination = document.getElementById("destinationDir");
+    var sub = document.getElementById("subDir");
+    sub.setAttribute("href", "/directionsRTN/?origin=" + origin.value+ "&destination=" + desination.value);
+}
---
+++ app/js/routes.js
@@ -0,0 +1,95 @@
+routes = [
+    {
+        path: '/home/',
+        url: 'index.html',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/disruptions/',
+        url: hostedDir() + '/disruptions.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/times/',
+        url: hostedDir() + '/times.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/directions/',
+        url: hostedDir() + '/directions.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/stop/',
+        url: hostedDir() + '/stop.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/dep/',
+        url: hostedDir() + '/dep.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/vdetails/',
+        url: hostedDir() + '/vdetails.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/directionsRTN/',
+        url: hostedDir() + '/directionsRTN.php',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/tickets/',
+        url: 'pages/tickets.html',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/map/',
+        url: 'pages/map.html',
+        options: {
+            pushState: true
+        }
+    },
+    {
+        path: '/help/',
+        url: 'pages/help.html',
+        options: {
+            pushState: true
+        }
+    },
+    // Default route (404 page). MUST BE THE LAST
+    {
+        path: '(.*)',
+        url: './pages/404.html',
+        options: {
+            pushState: true
+        }
+    }
+];
+function hostedDir() {
+    if (window.location.hostname == "adam.local") {
+        return "../hosted_app-V2";
+    } else {
+        return "http://cityrunner-server.genav.ga/hosted_app-V2/";
+    }
+
+}
